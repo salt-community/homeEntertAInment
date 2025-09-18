34e12f15c18ff0bf4da29182d0bda724e8384322
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-18 10:01:40 +0200

    initialising repo

---
+++ backend/src/test/java/com/bestgroup/HomeEntertAInment/HomeEntertAInmentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestgroup.HomeEntertAInment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeEntertAInmentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

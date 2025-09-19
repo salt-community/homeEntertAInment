568b365859f41da91e86375e1bcf04f7095dc76c
AmandaEmilia <amanda.e.strom@gmail.com>
2025-09-19 11:20:15 +0200

    Updating port for FE

--- backend/src/main/java/com/bestgroup/HomeEntertAInment/controller/StoryController.java
+++ backend/src/main/java/com/bestgroup/HomeEntertAInment/controller/StoryController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/story")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:5173")
 public class StoryController {
 
     private final StoryService storyService;

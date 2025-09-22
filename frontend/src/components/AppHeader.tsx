8ade8f9026844889911dec8c9dd9add16909d93e
Nikita Mokhonko <nikitamokhonko@gmail.com>
2025-09-22 14:02:51 +0200

    Implemented logo into website

--- frontend/src/components/AppHeader.tsx
+++ frontend/src/components/AppHeader.tsx
@@ -2,17 +2,22 @@ import { Link } from "@tanstack/react-router";
 
 export function AppHeader() {
   return (
-    <header className="w-full bg-gradient-to-r from-[#F930C7]/10 via-transparent to-[#3076F9]/10">
-      <div className="mx-auto w-full px-8 py-6">
+    <header className="w-full ">
+      <div className="mx-auto w-full px-8 py-8">
         <h1 className="text-2xl sm:text-3xl font-semibold tracking-wide">
           <Link to="/" className="text-left block">
-            <span className="text-2xl justify-center align-middle bg-gradient-to-r from-[#3076F9] to-[#F930C7] bg-clip-text text-transparent">
+            <span className="text-2xl justify-left gap-5 flex flex-row bg-gradient-to-r from-[#3076F9] to-[#F930C7] w-100 bg-clip-text text-transparent">
+              <img
+                src="/logo.png"
+                alt="Home Entertainment System Logo"
+                className="w-8 h-8"
+              />
               Home Entertainment System
             </span>
           </Link>
         </h1>
       </div>
-      <div className="h-[1px] mt-3 w-full bg-gradient-to-r from-[#F930C7] to-[#3076F9]" />
+      <div className="h-[1px] w-full bg-gradient-to-r from-[#F930C7] to-[#3076F9]" />
     </header>
   );
 }
